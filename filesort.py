713fc9f707df59c4ed2f7a2bfb0a2b67f2ae35a3
Jertzukka <Jertzukka@gmail.com>
2020-08-03 12:30:12 +0300

    Windows fixes

--- filesort.py
+++ filesort.py
@@ -32,8 +32,9 @@ if __name__ == '__main__':
         output = os.path.join(folder, string)
 
     destination, found, origin = ([] for i in range(3))
+    
     for file in os.listdir(folder):
-        if (extension == ""  or file.endswith(extension)) and string in file:
+        if file.endswith(extension) and string in file:
             found.append(file)
             destination.append(os.path.join(output,file))
 
@@ -51,34 +52,23 @@ if __name__ == '__main__':
 
     dialogue = input("We're moving {} files into {}\nAre you sure? (y/n/review)   ".format(str(len(found)), output))
     if dialogue == "review" or dialogue == "y":
-        if dialogue == "review":
-            temp = tempfile.NamedTemporaryFile('w+t')
-            if verbose: print("Temporary file created: {}".format(temp.name))
+        if dialogue == "review" and (len(found) > 0):
+            print("\n" + "-" * len(origin[0] + " -> " + destination[0]))
+            for i in range(0, len(origin)):
+                print(origin[i] + " -> " + destination[i])
+            print("-" * len(origin[0] + " -> " + destination[0]) + "\n")
+            cont = input("Finish moving? (y/n)   ")
+            if cont != "y":
+                print("Exiting...")
+                sys.exit(1)
 
-            try:
-                for i in range(0, len(origin)):
-                    temp.writelines(origin[i] + "   " + destination[i] + "\n")
-                temp.seek(0)
-
-                opener = "open" if sys.platform == "darwin" else "xdg-open"
-                #subprocess.call([opener, temp.name])
-                subprocess.Popen([opener,temp.name])
-
-                cont = input("Finish moving? (y/n)   ")
-                if cont != "y":
-                    print("Exiting...")
-                    sys.exit(1)
-            finally:
-                if verbose: print("Closing temporary file...")
-                temp.close()
-
-        if not os.path.exists(output): 
+        if not os.path.exists(str(output)): 
             os.makedirs(output)
             if verbose: print("Creating a folder '{}' as one does not exist.".format(output))
         if len(origin) == len(destination):
             for i in range(0, len(origin)):
                 os.rename(origin[i], destination[i])
-                print("Moving {} from {} to {}".format(str(found[i]),origin[i],destination[i]))
+                if verbose: print("Moving {} from {} to {}".format(str(found[i]),origin[i],destination[i]))
         else:
             print("Error, origin and destination count don't match!")
             sys.exit(1)
